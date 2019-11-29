f55a094a8880b7d1701f27cf2e82f9b794c59abc
Revingston <Revingston@yandex.ru>
2019-11-29 20:08:43 +0300

    25

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 const express = require("express");
-const bodyParser = require("body-parser");
- const app = express();
-  // создаем парсер для данных application/x-www-form-urlencoded
-const urlencodedParser = bodyParser.urlencoded({extended: false});
- app.get("/register", urlencodedParser, function (request, response) {
-    response.sendFile(__dirname + "/register.html");
-});
-app.post("/register", urlencodedParser, function (request, response) {
-    if(!request.body) return response.sendStatus(400);
-    console.log(request.body);
-    response.send(`${request.body.userName} - ${request.body.userAge}`);
-});
-  app.get("/", function(request, response){
-    response.send("Главная страница");
-
-});
-const express = require("express");
 const app = express();
- app.get("/products/:productId", function (request, response) {
-  response.send("productId: " + request.params["productId"])
+ app.use("/about", function (request, response) {
+  response.send("О сайте");
 });
- app.listen(3000);
+ app.use("/products/create",function (request, response) {
+  response.send("Добавление товара");
+});
+app.use("/products/:id",function (request, response) {
+  response.send(`Товар ${request.params.id}`);
+});
+app.use("/products/",function (request, response) {
+  response.send("Список товаров");
+});
+ app.use("/", function (request, response) {
+  response.send("Главная страница");
+});
+app.listen(3000);
+
 
