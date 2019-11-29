77fd253f8bb5e0ac531674743e8361e5c29283ed
Revingston <Revingston@yandex.ru>
2019-11-29 20:20:44 +0300

    29

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
-  const app = express();
-// создаем парсер для данных в формате json
-const jsonParser = express.json();
-  app.post("/user", jsonParser, function (request, response) {
-    console.log(request.body);
-    if(!request.body) return response.sendStatus(400);
-         response.json(request.body); // отправляем пришедший ответ обратно
-});
-  app.get("/", function(request, response){
-          response.sendFile(__dirname + "/index.html");
-});
-  app.listen(3000);
+const app = express();
+ // определяем роутеры
+const userRouter = express.Router();  // для адресов с "/users"
+ userRouter.use("/create", function (request, response){
+    response.send("Добавление пользователя");});
+userRouter.use("/", function(request, response){
+    response.send("Список пользователей");});
+// сопоcтавляем роутер с конечной точкой "/users"
+app.use("/users", userRouter);
+ // общие обработчики
+app.get("/about", function (request, response) {
+    response.send("О сайте");});
+app.get("/", function (request, response) {
+    response.send("Главная страница");});
+ // обработка ошибки 404
+app.use(function (req, res, next) {
+    res.status(404).send("Not Found")});
+ app.listen(3000);
