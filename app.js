9bd434768d6db48a33b7cfdc4371ab0b0187677a
Revingston <Revingston@yandex.ru>
2019-12-10 10:25:11 +0300

    1sql

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
-const express = require("express");
-const multer  = require("multer");
- const app = express();
- const storageConfig = multer.diskStorage({
-    destination: (req, file, cb) =>{
-        cb(null, "uploads");    },
-    filename: (req, file, cb) =>{
-        cb(null, file.originalname);    }
+const mysql = require("mysql2");
+ const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "Revingston",
+  password: ""
 });
-// определение фильтра
-const fileFilter = (req, file, cb) => {
-      if(file.mimetype === "image/png" || 
-    file.mimetype === "image/jpg"|| 
-    file.mimetype === "image/jpeg"){
-        cb(null, true);    }
-    else{
-        cb(null, false);    }
- }
- app.use(express.static(__dirname));
- app.use(multer({storage:storageConfig, fileFilter: fileFilter}).single("filedata"));
-app.post("/upload", function (req, res, next) {
-       let filedata = req.file;
-     console.log(filedata);
-    if(!filedata)
-        res.send("Ошибка при загрузке файла");
-    else
-        res.send("Файл загружен");
-});
-app.listen(3000, ()=>{console.log("Server started");});
+connection.connect(function(err){
+        if (err) {
+          return console.error("Ошибка: " + err.message);    }
+        else{
+          console.log("Подключение к серверу MySQL успешно установлено");    }
+     });
+     connection.end(function(err) {
+          if (err) {
+            return console.log("Ошибка: " + err.message);
+          }
+          console.log("Подключение закрыто");
+        });
+        
+    
