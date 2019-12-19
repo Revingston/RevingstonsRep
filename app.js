47ca7886fc6baca6743b2f9b3767c033b6440dbe
Revingston <Revingston@yandex.ru>
2019-12-19 20:31:02 +0300

    18sql

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
 const mysql = require("mysql2");
-/* const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "revingston",
-  password: ""
-});*/
 const pool = mysql.createPool({
         connectionLimit: 5,
         host: "localhost",
@@ -12,19 +6,17 @@ const pool = mysql.createPool({
         password: "", 
         database: "revingston"
     });
-    // добавление объекта
-const sql = "INSERT INTO users (name, age) VALUES(?, ?) ";
-const data = ["Bill", 25];
-pool.query(sql, data, function(err, results) {
-  if(err) console.log(err);
-  console.log(results);
-});
- 
-// получение объектов
-pool.query("SELECT * FROM users", function(err, results) {
-    if(err) console.log(err);
-    console.log(results);
-});
+    pool.query("SELECT * FROM users", function(err, results) {
+            if(err) console.log(err);
+            console.log(results);
+        });
+         pool.end(function(err) {
+          if (err) {
+            console.log(err.message);
+          }
+          console.log("пул закрыт");
+        });
+        
 connection.end();
 
     
