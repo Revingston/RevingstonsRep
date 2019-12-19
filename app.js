259e6659a4218b661e468128f8b1af0ac8c99c90
Revingston <Revingston@yandex.ru>
2019-12-19 20:28:33 +0300

    17sql

--- app.js
+++ app.js
@@ -12,6 +12,21 @@ const pool = mysql.createPool({
         password: "", 
         database: "revingston"
     });
+    // добавление объекта
+const sql = "INSERT INTO users (name, age) VALUES(?, ?) ";
+const data = ["Bill", 25];
+pool.query(sql, data, function(err, results) {
+  if(err) console.log(err);
+  console.log(results);
+});
+ 
+// получение объектов
+pool.query("SELECT * FROM users", function(err, results) {
+    if(err) console.log(err);
+    console.log(results);
+});
+connection.end();
+
     
 
 
